7927abc991fdb053de2b391e73ead7e87d8e6f26
thanachot30 <su.thanachot@gmail.com>
2022-04-17 22:22:10 +0700

    add live in life

--- test_python.py
+++ test_python.py
@@ -1,3 +1,5 @@
-import cv2 
+import cv2
 
 print("thanachot with opencv and now i interresting about data engineer to make my is happy and good lift better now")
+
+print("and now i live in my head ,learning in my emotion and think , next am i ,,")
